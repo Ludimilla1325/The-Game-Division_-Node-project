e682c22eec7612b5f451b42ad565aff683496dd4
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-01-20 23:46:06 -0300

    Trying again to push to heroku :(

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cookieParser = require ('cookie-parser');
 const moment = require('moment');
 const config = require ('./config/config').get(process.env.NODE_ENV);
 const app = express();
-
+require("dotenv").config();
 
 // ## HBS SETUP ##
 app.engine('hbs',hbs({
